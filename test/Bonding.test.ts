4662061bd3e31076953eeefaca2460b1c20f7417
Yasin <64729564+kollegian@users.noreply.github.com>
2025-04-22 22:01:23 +0300

    Tests: Extend existing tests (#4)

--- test/Bonding.test.ts
+++ test/Bonding.test.ts
@@ -1,8 +1,22 @@
-import { expect } from "chai";
-import { ethers, upgrades } from "hardhat";
-import { Contract, Interface, Signer } from "ethers";
-import { network } from "hardhat"
-import { getGraduatedEvent, getLaunchedEvent } from "./utils";
+import {expect} from "chai";
+import {ethers, network, upgrades} from "hardhat";
+import {Interface, Signer} from "ethers";
+import {
+    calculateAmountOutForBuy, calculateAmountOutForSell,
+    formatBalance,
+    getGraduatedEvent,
+    getLaunchedEvent,
+    getLaunchedTokenAndPairContracts
+} from "./utils";
+import {
+    Bonding,
+    FERC20,
+    FFactory,
+    FRouter,
+    IDragonswapFactory,
+    IDragonswapRouter,
+    WSEI,
+} from "../typechain-types";
 
 type DragonSwapAddresses = {
     factory: string;
@@ -16,12 +30,21 @@ const dragonSwapContracts: Record<string, DragonSwapAddresses> = {
     },
 };
 
+const BUY_TAX = 5;
+const SELL_TAX = 10;
+const SEI_LAUNCH_FEE = ethers.parseEther("100");
+const ASSET_LAUNCH_FEE = ethers.parseEther("200");
+const MAX_TX = 20;
+const INITIAL_SUPPLY = ethers.parseEther("100000");
+const GRAD_THRESHOLD = ethers.parseEther("25000");
+const MULTIPLIER = 5000;
+
+
 describe("Bonding Contract", function () {
     let owner: Signer, user: Signer, feeRecipient: Signer;
-    let Bonding: Contract, Factory: Contract, Router: Contract, AssetToken: Contract, WSEI: Contract, DragonswapRouter: Contract, DragonswapFactory: Contract;
+    let Bonding: Bonding, Factory: FFactory, Router: FRouter, AssetToken: FERC20, WSEI: WSEI, DragonswapRouter: IDragonswapRouter, DragonswapFactory: IDragonswapFactory;
     let PairAbi: Interface
-
-    let GraduatedToken: string, AssetTradedToken: string, AssetTradedPair: string
+    let GraduatedToken: string, AssetTradedToken: string, AssetTradedPair: string;
 
     before(async function () {
         [owner, user, feeRecipient] = await ethers.getSigners();
@@ -50,8 +73,7 @@ describe("Bonding Contract", function () {
 
         // Deploy Factory contract
         const FFactory = await ethers.getContractFactory("FFactory");
-        const multiplier = 5000
-        Factory = await upgrades.deployProxy(FFactory, [await feeRecipient.getAddress(), 5, 5, multiplier], { initializer: "initialize" });
+        Factory = await upgrades.deployProxy(FFactory, [await feeRecipient.getAddress(), BUY_TAX, SELL_TAX, MULTIPLIER], { initializer: "initialize" });
         await Factory.waitForDeployment();
 
         // Deploy Router contract
@@ -83,20 +105,17 @@ describe("Bonding Contract", function () {
         Bonding = await upgrades.deployProxy(BondingFactory, [], { initializer: false });
         await Bonding.waitForDeployment();
 
-        const initialSupply = ethers.parseEther("100000")
-        const gradThreshold = ethers.parseEther("25000")
-        const maxTx = 20
         // Initialize Bonding contract
         await Bonding.initialize(
             Factory.target,   // factory address
             Router.target,    // router address
             WSEI.target,      // address of WSEI contract that helps us wrap SEI
-            ethers.parseEther("102"),               // asset launch fee amount
-            ethers.parseEther("100"),               // sei launch fee amount
-            initialSupply, // initial supply
-            maxTx, // maximum percentage of each token that can be bought in one tx.
-            gradThreshold, // sei grad threshold
-            gradThreshold, // asset grad threshold
+            ASSET_LAUNCH_FEE,               // asset launch fee amount
+            SEI_LAUNCH_FEE,               // sei launch fee amount
+            INITIAL_SUPPLY, // initial supply
+            MAX_TX, // maximum percentage of each token that can be bought in one tx.
+            GRAD_THRESHOLD, // sei grad threshold
+            GRAD_THRESHOLD, // asset grad threshold
             100, // Sets the dragonswap tax bps (100 is 1%)
             dragonswapFactory,
             dragonswapRouter
@@ -127,6 +146,23 @@ describe("Bonding Contract", function () {
         expect(await Bonding.maxTx()).to.equal("100");
     });
 
+    it('Shouldnt allow user to to set initial supply and graduation threshold', async function(){
+
+        await expect(Bonding.connect(user).setInitialSupply(ethers.parseEther("1000000000")))
+            .to.be.reverted;
+
+        await expect(Bonding.connect(user).setSeiGradThreshold(ethers.parseEther("100000")))
+            .to.be.reverted;
+
+        await expect(Bonding.connect(user).setAssetGradThreshold(ethers.parseEther("100000")))
+            .to.be.reverted;
+    });
+
+    it('Shouldnt allow user to set max tx', async function(){
+        await expect(Bonding.connect(user).setMaxTx(ethers.parseEther('100')))
+            .to.be.reverted;
+    });
+
     it("should create a trading pair", async function () {
         // Create a test ERC20 token
         const TestToken = await ethers.getContractFactory("FERC20");
@@ -145,15 +181,18 @@ describe("Bonding Contract", function () {
         expect(pairAddress).to.properAddress;
     });
 
-    it("should allow user to launch a token", async function () {
+    it("should allow user to launch a token with asset amount equal to launch fee", async function () {
+        await Bonding.setInitialSupply(INITIAL_SUPPLY);
+        await Bonding.setSeiGradThreshold(GRAD_THRESHOLD);
+        await Bonding.setAssetGradThreshold(GRAD_THRESHOLD);
+
         // Send some token from owner to user
-        const launchFee = await Bonding.assetLaunchFee()
-        const initialPurchase = launchFee
+        const initialPurchase = await Bonding.assetLaunchFee();
         const transferResult = await AssetToken.connect(owner).transfer(await user.getAddress(), initialPurchase)
-        await transferResult.wait()
+        await transferResult.wait();
         // User approves Bonding contract to spend AssetToken so it can seed the liquidity pool with the initial purchase.
         const approveResult = await AssetToken.connect(user).approve(Bonding.target, initialPurchase);
-        await approveResult.wait()
+        await approveResult.wait();
 
         // Launch a token
         const tx = await Bonding.connect(user).launchWithAsset(
@@ -163,7 +202,6 @@ describe("Bonding Contract", function () {
             AssetToken.target
         );
 
-        const receipt = await tx.wait();
         const filter = Bonding.filters.Launched();
         const events = await Bonding.queryFilter(filter, "latest");
 
@@ -178,6 +216,203 @@ describe("Bonding Contract", function () {
         const tokenContract = await ethers.getContractAt("FERC20", AssetTradedToken, user);
         const currBal = await tokenContract.balanceOf(await user.getAddress())
         expect(currBal).to.be.equals(0);
+    });
+
+    it('Shouldnt allow a user to launch a token with asset amount less than launch fee', async function (){
+        const lessThanLaunchFee = ethers.parseEther((formatBalance(await Bonding.assetLaunchFee()) - 1).toString());
+        const transferResult = await AssetToken.connect(owner).transfer(await user.getAddress(), lessThanLaunchFee)
+        await transferResult.wait();
+        // User approves Bonding contract to spend AssetToken so it can seed the liquidity pool with the initial purchase.
+        const approveResult = await AssetToken.connect(user).approve(Bonding.target, lessThanLaunchFee);
+        await approveResult.wait();
+
+        await expect(Bonding.connect(user).launchWithAsset(
+            "Test Token",
+            "TST",
+            lessThanLaunchFee,
+            AssetToken.target
+        )).to.be.revertedWith("Purchase amount must be greater than or equal to fee");
+    });
+
+    it('Should perform a buy if the launch amount is higher than the launch fee', async function (){
+        const moreThanFeeAmount = 100;
+        const moreThanLaunchFee = ethers.parseEther((formatBalance(await Bonding.assetLaunchFee())
+            + moreThanFeeAmount).toString());
+
+        const transferResult = await AssetToken.connect(owner)
+            .transfer(await user.getAddress(), moreThanLaunchFee)
+        await transferResult.wait();
+        // User approves Bonding contract to spend AssetToken so it can seed the liquidity pool with the initial purchase.
+        const approveResult = await AssetToken.connect(user)
+            .approve(Bonding.target, moreThanLaunchFee);
+        await approveResult.wait();
+
+        await Bonding.connect(user).launchWithAsset(
+            "Test Token",
+            "TST",
+            moreThanLaunchFee,
+            AssetToken.target
+        );
+
+        const {launchedTokenContract} = await getLaunchedTokenAndPairContracts(Bonding);
+        const launcherAfterBalance = await launchedTokenContract.balanceOf(await user.getAddress());
+        const expectedAmountOfToken = calculateAmountOutForBuy(moreThanFeeAmount, ASSET_LAUNCH_FEE, INITIAL_SUPPLY, MULTIPLIER, BUY_TAX);
+
+        expect(formatBalance(launcherAfterBalance)).to.be.equal(expectedAmountOfToken);
+    });
+
+    it('Should allow a launch that directly exceeds grad threshold', async function (){
+        const expectedOverGradAmount = formatBalance(GRAD_THRESHOLD) + formatBalance(ASSET_LAUNCH_FEE);
+        const launchAmountWithTax = ethers.parseEther(((expectedOverGradAmount / (1 - (BUY_TAX/100)) + 5).toString()));
+
+        const transferResult = await AssetToken.connect(owner)
+            .transfer(await user.getAddress(), launchAmountWithTax)
+        await transferResult.wait();
+
+        await AssetToken.connect(user).approve(Bonding.target, launchAmountWithTax);
+        await Bonding.connect(user).launchWithAsset(
+            "Test Token",
+            "TST",
+            launchAmountWithTax,
+            AssetToken.target
+        );
+        const {launchedTokenContract, syntheticPairContract} = await getLaunchedTokenAndPairContracts(Bonding);
+        const [tokenReserve, assetReserve] = await syntheticPairContract.getReserves();
+        expect(formatBalance(tokenReserve)).to.be.equal(0);
+        expect(formatBalance(assetReserve)).to.be.equal(0);
+
+        const tokenInfo = await Bonding.tokenInfo(launchedTokenContract.target);
+        expect(tokenInfo.trading).to.be.false;
+        expect(tokenInfo.tradingOnDragonswap).to.be.true;
+    });
+
+    it('Token info returns correct information about launched token', async function (){
+        const launchAmount = ethers.parseEther('500');
+        const transferResult = await AssetToken.connect(owner)
+            .transfer(await user.getAddress(), launchAmount)
+        await transferResult.wait();
+
+        await AssetToken.connect(user).approve(Bonding.target, launchAmount);
+        await Bonding.connect(user).launchWithAsset(
+            "Test Token",
+            "TST",
+            launchAmount,
+            AssetToken.target
+        );
+        const {launchedTokenContract, syntheticPairContract} = await getLaunchedTokenAndPairContracts(Bonding);
+        const tokenInfo = await Bonding.tokenInfo(launchedTokenContract.target);
+        expect(tokenInfo.creator).to.be.eq(await user.getAddress());
+        expect(tokenInfo.token).to.be.eq(launchedTokenContract.target);
+        expect(tokenInfo.pair).to.be.eq(syntheticPairContract.target);
+        expect(tokenInfo.trading).to.be.true;
+        expect(tokenInfo.tradingOnDragonswap).to.be.false;
+
+        const tokenData = tokenInfo.data;
+        expect(tokenData.supply).to.be.eq(INITIAL_SUPPLY);
+        expect(tokenData.token).to.be.eq(launchedTokenContract.target);
+        expect(tokenData.name).to.be.eq("aiden Test Token");
+    });
+
+    it('Token info correctly tracks volume with buy and sell operations', async function (){
+        const launchAmount = ethers.parseEther('200');
+        const transferResult = await AssetToken.connect(owner)
+            .transfer(await user.getAddress(), launchAmount)
+        await transferResult.wait();
+
+        await AssetToken.connect(user).approve(Bonding.target, launchAmount);
+        await Bonding.connect(user).launchWithAsset(
+            "Test Token",
+            "TST",
+            launchAmount,
+            AssetToken.target
+        );
+
+        const {launchedTokenContract, syntheticPairContract} = await getLaunchedTokenAndPairContracts(Bonding);
+        let tokenInfo = await Bonding.tokenInfo(launchedTokenContract.target);
+        //Since no buy current volume should be 0
+        expect(tokenInfo.data.volume).to.be.eq(0);
+
+        //Owner buys for 20 asset token. Minus fee it should be 19 volume now
+        await AssetToken.approve(Bonding.target, ethers.parseEther('20'));
+        await Bonding.buyWithAsset(ethers.parseEther('20'), launchedTokenContract.target, AssetToken.target);
+        tokenInfo = await Bonding.tokenInfo(launchedTokenContract.target);
+        expect(tokenInfo.data.volume).to.be.eq(ethers.parseEther('19'));
+
+        //Owner sells for 1 launched token
+        const [tokenBalance, assetBalance ] = await syntheticPairContract.getReserves();
+        const ownerPreBalance = await AssetToken.balanceOf(await owner.getAddress());
+        const expectedReturnAmount = calculateAmountOutForSell(1, tokenBalance, assetBalance, MULTIPLIER, SELL_TAX);
+
+        await launchedTokenContract.approve(Bonding.target, ethers.parseEther('10'));
+        await Bonding.sellForAsset(ethers.parseEther('1'), launchedTokenContract.target, AssetToken.target);
+        const ownerAfterBalance = await AssetToken.balanceOf(await owner.getAddress());
+
+        //Validate sell amount with multiplier
+        const receivedAssetAmount = formatBalance(ownerAfterBalance) - formatBalance(ownerPreBalance);
+        expect(receivedAssetAmount.toFixed(5)).to.be.equal(expectedReturnAmount.toString());
+        tokenInfo = await Bonding.tokenInfo(launchedTokenContract.target);
+        const expectedVolumeAmount = calculateAmountOutForSell(1, tokenBalance, assetBalance, MULTIPLIER, SELL_TAX);
+        expect(Math.abs(formatBalance(tokenInfo.data.volume) - (expectedVolumeAmount + 19))).to.be.within(0.5, 1.5);
+    });
+
+    it('Token info returns correct price', async function (){
+        const launchAmount = ethers.parseEther('400');
+        const transferResult = await AssetToken.connect(owner)
+            .transfer(await user.getAddress(), launchAmount)
+        await transferResult.wait();
+
+        await AssetToken.connect(user).approve(Bonding.target, ethers.parseEther('1000'));
+        await Bonding.connect(user).launchWithAsset(
+            "Test Token",
+            "TST",
+            launchAmount,
+            AssetToken.target
+        );
+
+        const {launchedTokenContract, syntheticPairContract} = await getLaunchedTokenAndPairContracts(Bonding);
+        const initialPrice = calculateAmountOutForSell(1, INITIAL_SUPPLY, launchAmount, MULTIPLIER, 1);
+        let tokenInfo = await Bonding.tokenInfo(launchedTokenContract.target);
+        expect(Math.abs(formatBalance(tokenInfo.data.price) - initialPrice)).to.be.within(0, 0.4);
+
+        //After a buy price is correct with multiplier
+        await Bonding.connect(user).buyWithAsset(ethers.parseEther('10'), launchedTokenContract.target, AssetToken.target);
+        const [tokenBalance, assetBalance ] = await syntheticPairContract.getReserves();
+        const intermediatePrice = calculateAmountOutForSell(1, tokenBalance, assetBalance, MULTIPLIER, 1);
+        const priceAfterBuy = await Bonding.tokenInfo(launchedTokenContract.target);
+        expect(Math.abs(formatBalance(priceAfterBuy.data.price) - intermediatePrice)).to.be.within(0, 0.4);
+    });
+
+    it('Updating grad threshold on existing pools doesnt cause pools to be graduated', async function (){
+        // Existing threshold is 25000
+        const launchAmount = ethers.parseEther('400');
+        const transferResult = await AssetToken.connect(owner)
+            .transfer(await user.getAddress(), launchAmount)
+        await transferResult.wait();
+
+        await AssetToken.connect(user).approve(Bonding.target, ethers.parseEther('1000'));
+        await Bonding.connect(user).launchWithAsset(
+            "Test Token",
+            "TST",
+            launchAmount,
+            AssetToken.target
+        );
+
+        const {launchedTokenContract, syntheticPairContract} = await getLaunchedTokenAndPairContracts(Bonding);
+        await Bonding.setAssetGradThreshold(ethers.parseEther('300'));
+        let tokenInfo = await Bonding.tokenInfo(launchedTokenContract);
+        expect(tokenInfo.trading).to.be.true;
+
+        //Next trade sends it to graduated
+        await Bonding.connect(user).buyWithAsset(ethers.parseEther('10'), launchedTokenContract.target, AssetToken.target);
+        tokenInfo = await Bonding.tokenInfo(launchedTokenContract);
+        expect(tokenInfo.trading).to.be.false;
+
+        //Reserves are empty
+        const [tokenBalance, assetBalance ] = await syntheticPairContract.getReserves();
+        expect(tokenBalance).to.be.eq(0);
+        expect(assetBalance).to.be.eq(0);
+
+        await Bonding.setAssetGradThreshold(ethers.parseEther('25000'));
     });
 
     it("should allow a user to buy and sell tokens", async function () {
@@ -434,10 +669,12 @@ describe("Bonding Contract", function () {
 
     // Test max tx (max percentage of token that can be bought at once)
     it("Should limit the amount of token that can be traded based on maxTx", async function () {
-        const maxPercentage = 10
+        const maxPercentage = 10;
         await Bonding.setMaxTx(maxPercentage);
         expect(await Bonding.maxTx()).to.equal(maxPercentage);
-        
+        await Bonding.setInitialSupply(ethers.parseEther("1000000000"));
+        await Bonding.setSeiGradThreshold(ethers.parseEther("100000"));
+
         const maxLaunchInput = await Bonding.getMaxLaunchInputSei();
         console.log("maximum", ethers.formatEther(maxLaunchInput));
 
--- test/utils.ts
+++ test/utils.ts
@@ -1,4 +1,6 @@
-import { Interface, Log } from "ethers";
+import {BigNumberish, Interface, Log} from "ethers";
+import {Bonding} from "../typechain-types";
+import {ethers} from "hardhat";
 
 export const getLaunchedEvent = (receipt: any): any => {
 
@@ -52,4 +54,58 @@ export const getGraduatedEvent = (receipt: any): any => {
         const parsed = iface.parseLog(log);
         return parsed
     }
+}
+
+export const formatBalance = (balance: BigNumberish): number => {
+    const etherString = ethers.formatEther(balance);
+    const etherValue = parseFloat(etherString);
+    return parseFloat(etherValue.toFixed(5));
+};
+
+export const calculateAmountOutForBuy = (
+    amountIn: number,
+    inReserve: BigNumberish,
+    outReserve: BigNumberish,
+    multiplier: number,
+    buyTax: number
+): number => {
+    amountIn = amountIn * (1 - (buyTax / 100));
+    const reserveIn =  multiplier * parseFloat(ethers.formatEther(inReserve));
+    const reserveOut = parseFloat(ethers.formatEther(outReserve));
+    const amountOut = (reserveOut * amountIn) / (amountIn + reserveIn);
+    return parseFloat(amountOut.toFixed(5));
+};
+
+export const calculateAmountOutForSell = (
+    amountIn: number,
+    inReserve: BigNumberish,
+    outReserve: BigNumberish,
+    multiplier: number,
+    sellTax: number
+) => {
+    const reserveIn =  parseFloat(ethers.formatEther(inReserve));
+    const reserveOut = multiplier * parseFloat(ethers.formatEther(outReserve));
+    const amountOut = (reserveOut * amountIn) / (amountIn + reserveIn);
+    return parseFloat((amountOut - (amountOut * (sellTax / 100))).toFixed(5));
+}
+
+export function calculateAmountInForExactOutput(amountOut: number, inReserve: BigNumberish, outReserve: BigNumberish, multiplier: number, tax: number){
+    const reserveIn = multiplier * parseFloat(ethers.formatEther(inReserve));
+    const reserveOut = parseFloat(ethers.formatEther(outReserve));
+
+    if (amountOut >= reserveOut) {
+        throw new Error("Desired output amount must be less than the available output reserve");
+    }
+    const netAmountIn = (amountOut * reserveIn) / (reserveOut - amountOut);
+    const amountIn = netAmountIn / (1 - (tax/100));
+    return parseFloat(amountIn.toFixed(5));
+}
+
+export async function getLaunchedTokenAndPairContracts(contract: Bonding){
+    const filter = contract.filters.Launched();
+    const events = await contract.queryFilter(filter, "latest");
+    const parsedEvents = contract.interface.parseLog(events[0]);
+    const launchedTokenContract = await ethers.getContractAt("ERC20", parsedEvents!.args.token);
+    const syntheticPairContract = await ethers.getContractAt("SyntheticPair", parsedEvents!.args.pair);
+    return {launchedTokenContract, syntheticPairContract};
 }
